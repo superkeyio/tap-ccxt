6e3a13c82c92d7b2340a9edd6c1ebfae9703af3d
Matt Evenson <mevenson@usc.edu>
2022-04-27 14:43:51 -0700

    sort of works but state updates are too infrequent

--- tap_ccxt/streams.py
+++ tap_ccxt/streams.py
@@ -7,27 +7,26 @@ from singer_sdk import typing as th  # JSON Schema typing helpers
 
 from tap_ccxt.client import ccxtStream
 from ccxt.base.exchange import Exchange
+from datetime import datetime
+import math
 
 # TODO: Delete this is if not using json files for schema definition
-SCHEMAS_DIR = Path(__file__).parent / Path("./schemas")
+# SCHEMAS_DIR = Path(__file__).parent / Path("./schemas")
 # TODO: - Override `UsersStream` and `GroupsStream` with your own stream definition.
 #       - Copy-paste as many times as needed to create multiple stream types.
 
+
 class OHLCVStream(ccxtStream):
-    name = 'ohlcv'
-    
+    name = "ohlcv"
+
     replication_key = "timestamp"
 
     exchange: Exchange = None
-    symbol: str = None
+    symbols: List[str] = []
     timeframe: str = None
 
     schema = th.PropertiesList(
-        th.Property(
-            "timestamp",
-            th.DateTimeType,
-            required=True
-        ),
+        th.Property("timestamp", th.DateTimeType, required=True),
         th.Property("timeframe", th.StringType),
         th.Property(
             "open",
@@ -54,14 +53,41 @@ class OHLCVStream(ccxtStream):
             th.NumberType,
             required=True,
         ),
-        th.Property(
-            "exchange_id",
-            th.StringType,
-            required=True
-        ),
-        th.Property(
-            "symbol",
-            th.StringType,
-            required=True
-        )
+        th.Property("exchange_id", th.StringType, required=True),
+        th.Property("symbol", th.StringType, required=True),
     ).to_dict()
+
+    def __init__(self, *args, **kwargs) -> None:
+        self.exchange = kwargs.pop("exchange")
+        self.symbols = kwargs.pop("symbols")
+        self.timeframe = kwargs.pop("timeframe")
+        super().__init__(*args, **kwargs)
+        self.STATE_MSG_FREQUENCY = 100
+
+    @property
+    def partitions(self) -> List[Dict[str, int]]:
+        return [{"symbol": symbol} for symbol in self.symbols]
+
+    def get_records(self, context: Optional[dict]) -> Iterable[dict]:
+        current_timestamp = math.floor(
+            self.get_starting_timestamp(context).timestamp() * 1000
+        )
+        end_timestamp = datetime.now().timestamp() * 1000
+        symbol = context.get("symbol")
+        # segment based on exchange?
+        while current_timestamp < end_timestamp:
+            candles = self.exchange.fetchOHLCV(
+                symbol, timeframe=self.timeframe, since=current_timestamp
+            )
+            for candle in candles:
+                yield dict(
+                    symbol=symbol,
+                    exchange_id=self.exchange.id,
+                    timestamp=datetime.fromtimestamp(candle[0] / 1000.0),
+                    open=candle[1],
+                    high=candle[2],
+                    low=candle[3],
+                    close=candle[4],
+                    volume=candle[5],
+                )
+                current_timestamp = candle[0]
--- tap_ccxt/tap.py
+++ tap_ccxt/tap.py
@@ -1,18 +1,20 @@
 """ccxt tap class."""
 
-from msilib.schema import Property
 from typing import List
 
 from singer_sdk import Tap, Stream
 from singer_sdk import typing as th  # JSON schema typing helpers
+
 # TODO: Import your custom stream types here:
 from tap_ccxt.streams import (
     OHLCVStream,
 )
+import ccxt
 
 
 class Tapccxt(Tap):
     """ccxt tap class."""
+
     name = "tap-ccxt"
 
     # TODO: Update this section with the actual config values you expect:
@@ -21,11 +23,7 @@ class Tapccxt(Tap):
             "exchanges",
             th.ArrayType(
                 th.ObjectType(
-                    th.Property(
-                        "id",
-                        th.StringType,
-                        required=True
-                    ),
+                    th.Property("id", th.StringType, required=True),
                     th.Property(
                         "timeframe",
                         th.StringType,
@@ -46,14 +44,30 @@ class Tapccxt(Tap):
                     ),
                 )
             ),
-            required=True
+            required=True,
         ),
-        th.Property(
-            "start_date",
-            th.DateTimeType
-        )
+        th.Property("start_date", th.DateTimeType),
     ).to_dict()
 
     def discover_streams(self) -> List[Stream]:
         """Return a list of discovered streams."""
         # status
+        streams = []
+        # maybe I am overthinking this
+        # exchange stream?
+        for exchange_config in self.config.get("exchanges"):
+            exchange_class = getattr(ccxt, exchange_config.get("id"))
+            exchange = exchange_class(
+                {
+                    "apiKey": exchange_config.get("api_key"),
+                    "secret": exchange_config.get("secret"),
+                }
+            )
+            ohlcv_stream = OHLCVStream(
+                tap=self,
+                exchange=exchange,
+                timeframe=exchange_config.get("timeframe"),
+                symbols=exchange_config.get("symbols"),
+            )
+            streams.append(ohlcv_stream)
+        return streams
